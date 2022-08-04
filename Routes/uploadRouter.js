2822f40cdc196884fb944f4e117abda13a5d87cd
quvi007 <1805007@ugrad.cse.buet.ac.bd>
2022-08-04 16:55:35 +0600

    new stuff

--- Routes/uploadRouter.js
+++ Routes/uploadRouter.js
@@ -3,8 +3,8 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send(req.files)
+    console.log(req)
+    res.send('File received')
 })
 
 module.exports = router
