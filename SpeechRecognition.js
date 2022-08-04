003b0fe7f1465b8faff982583e216fe0a1d2dd23
quvi007 <1805007@ugrad.cse.buet.ac.bd>
2022-08-04 13:15:06 +0600

    audio

---
+++ SpeechRecognition.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const sdk = require("microsoft-cognitiveservices-speech-sdk");
+const speechConfig = sdk.SpeechConfig.fromSubscription("7e216866b7b044fe963494935f9abbe3", "centralus");
+speechConfig.speechRecognitionLanguage = "en-US";
+
+function fromFile(res) {
+    let audioConfig = sdk.AudioConfig.fromWavFileInput(fs.readFileSync("audio.wav"));
+    let speechRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+
+     speechRecognizer.recognizeOnceAsync(result => {
+        switch (result.reason) {
+            case sdk.ResultReason.RecognizedSpeech:
+                res.send(`RECOGNIZED: Text=${result.text}`);
+                break;
+            case sdk.ResultReason.NoMatch:
+                res.send("NOMATCH: Speech could not be recognized.");
+                break;
+            case sdk.ResultReason.Canceled:
+                const cancellation = sdk.CancellationDetails.fromResult(result);
+                res.send(`CANCELED: Reason=${cancellation.reason}`);
+
+                if (cancellation.reason == sdk.CancellationReason.Error) {
+                    //res.send(`CANCELED: ErrorCode=${cancellation.ErrorCode}`);
+                    res.send(`CANCELED: ErrorDetails=${cancellation.errorDetails}`);
+                    //console.log("CANCELED: Did you set the speech resource key and region values?");
+                }
+                break;
+        }
+        speechRecognizer.close();
+    });
+}
+
+module.exports = fromFile;
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const fromFile = require('./SpeechRecognition.js')
 
 app.get('/', (req, res) => {
     res.send('Hello There')
+})
+
+app.get('/test-audio', async (req, res) => {
+    fromFile(res)
 })
 
 app.listen(process.env.PORT || 3000)
