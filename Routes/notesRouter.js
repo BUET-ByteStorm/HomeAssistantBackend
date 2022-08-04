62e2e496acca995153716848a0215dd9f487cfe1
quvi007 <1805007@ugrad.cse.buet.ac.bd>
2022-08-05 04:53:22 +0600

    Notes

--- Routes/notesRouter.js
+++ Routes/notesRouter.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-notes = []
+notes = [{noteCreator: 'Quvi', noteId: '1', noteContent: 'Test'}]
 
 router.post('/', (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-    
-
-    
     notes.push({noteCreator: req.body.noteCreator, noteId: req.body.noteId, noteContent: req.body.noteContent})
     res.send(`Received Note from ${ip}`)
 })
