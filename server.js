9008b7a961ee96084b13785752f2080e0a3aa995
quvi007 <1805007@ugrad.cse.buet.ac.bd>
2022-08-04 14:02:17 +0600

    rr

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const fileUpload = require('express-fileupload')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
 const app = express()
 const fromFile = require('./SpeechRecognition.js')
+const uploadRouter = require('./Routes/uploadRouter.js')
+
+app.use('/upload-file', uploadRouter)
+
+app.use(fileUpload())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : true}))
 
 app.get('/', (req, res) => {
     res.send('Hello There')
