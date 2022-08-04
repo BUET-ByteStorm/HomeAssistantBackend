7fea640334c075510f38e2e8e4dd2478a8f7f5d5
Aszad Rakin <rakinkzs@gmail.com>
2022-08-05 05:30:03 +0600

    database

---
+++ authentication/authentication.middleware.js
@@ -0,0 +1,74 @@
+import { NextFunction, Response } from 'express';
+
+import jwt from 'jsonwebtoken';
+import AuthRepository from '../database/repository/authRepository';
+const authRepository = new AuthRepository();
+
+import { isPasswordValid } from './authentication.service';
+
+const login = async (req, res, next) => {
+  const { username, password } = req.body;
+  // Fetching userData from database
+  const userInfo = await authRepository.fetchUser(username);
+  console.log(userInfo.data);
+
+  if (!userInfo.success)
+    return res.status(400).json({
+      success: false,
+      message: userInfo.error,
+    });
+  if (userInfo.data.length === 0)
+    return res.status(409).json({
+      success: false,
+      message: 'User not found',
+    });
+
+  const hashPass = userInfo.data[0].password;
+  //  Add more info if needed
+  const userId = userInfo.data[0].id;
+  console.log(hashPass);
+  console.log(isPasswordValid(hashPass, password));
+
+  if (hashPass && isPasswordValid(hashPass, password)) {
+    req.body = {
+      userId: userId,
+      username: username,
+    };
+    next();
+  } else {
+    res.status(400).json({
+      message: 'Invalid email or password',
+    });
+  }
+};
+
+const isValidJWTToken = (req, res, next) => {
+  const SECRET_KEY = process.env.JWT_SECRET || '';
+  if (req.headers['authorization']) {
+    try {
+      const authorization = req.headers['authorization'].split(' ');
+      if (authorization[0] != 'Bearer') {
+        return res.status(401).json({});
+      } else {
+        const userData = jwt.verify(authorization[1], SECRET_KEY);
+        //    add necessary data to request body. Here i added only userId
+        console.log(userData);
+
+        req.body.userId = userData?.userId;
+        req.body.username = userData?.username;
+
+        next();
+      }
+    } catch (err) {
+      console.log(err);
+
+      return res.status(403).send();
+    }
+  } else {
+    return res
+      .status(401)
+      .send({ error: 'Please attach access token in headers.' });
+  }
+};
+
+export { login, isValidJWTToken };
