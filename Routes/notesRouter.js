b23a448a897537f9df49bd3f742afdcc60554a59
quvi007 <1805007@ugrad.cse.buet.ac.bd>
2022-08-05 08:06:39 +0600

    backend

--- Routes/notesRouter.js
+++ Routes/notesRouter.js
@@ -4,15 +4,8 @@ const NoteController = require('../controllers/noteController');
 const noteController = new NoteController();
 const router = express.Router()
 
-notes = [{noteCreator: 'Quvi', noteId: '1', noteContent: 'Test'}]
+notes = []
 
-<<<<<<< HEAD
-router.post('/', (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-    notes.push({noteCreator: req.body.noteCreator, noteId: req.body.noteId, noteContent: req.body.noteContent})
-    res.send(`Received Note from ${ip}`)
-})
-=======
 router.use(isValidJWTToken);
 
 // router.post('/', (req, res) => {
@@ -25,7 +18,6 @@ router.use(isValidJWTToken);
 //     res.send(`Received Note from ${ip}`) ;
 // })
 router.post('/', noteController.addNote) ;
->>>>>>> db891b136e2bd6376a29fc8d73cc0d2905008964
 
 // router.get('/', (req, res) => {
 //     res.send(notes)
