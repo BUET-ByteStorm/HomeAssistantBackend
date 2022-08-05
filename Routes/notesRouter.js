dbb1986b07e3e68affc21426f07ed3d0813baaf8
Aszad Rakin <rakinkzs@gmail.com>
2022-08-05 07:17:45 +0600

    note controller, router, repository

--- Routes/notesRouter.js
+++ Routes/notesRouter.js
@@ -1,22 +1,27 @@
 const express = require('express')
 const { isValidJWTToken } = require('../authentication/authentication.middleware.js')
+const NoteController = require('../controllers/noteController');
+const noteController = new NoteController();
 const router = express.Router()
 
 notes = []
 
 router.use(isValidJWTToken);
 
-router.post('/', (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+// router.post('/', (req, res) => {
+//     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
     
 
     
-    notes.push({noteCreator: req.body.noteCreator, noteId: req.body.noteId, noteContent: req.body.noteContent})
-    res.send(`Received Note from ${ip}`)
-})
+//     notes.push({noteCreator: req.body.noteCreator, noteId: req.body.noteId, noteContent: req.body.noteContent}) ;
+//     noteRepository.addNote(userId,req.body.noteContent) ;
+//     res.send(`Received Note from ${ip}`) ;
+// })
+router.post('/', noteController.addNote) ;
 
-router.get('/', (req, res) => {
-    res.send(notes)
-})
+// router.get('/', (req, res) => {
+//     res.send(notes)
+// }) ;
+router.get('/', noteController.getUserNote) ;
 
 module.exports = router
